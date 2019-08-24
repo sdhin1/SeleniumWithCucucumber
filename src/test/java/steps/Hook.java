21528ddbb3df8759a4aeae87b08a029ac3a1bf24
Saurabh Dhingra <sdhingra13@gmail.com>
2019-08-24 17:14:04 +0530

    resolved illegal escape character error
    resolved illegal escape character error

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -31,7 +31,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\D Drive\SapientData\Automation Toolkit\Tools\Selenium\drivers");
+        System.setProperty("webdriver.chrome.driver", "C:\\D Drive\\SapientData\\Automation Toolkit\\Tools\\Selenium\\drivers\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
